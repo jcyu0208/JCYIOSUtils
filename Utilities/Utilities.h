73a79140fbe20a14101bb54f6a386b7ca554ef97
Juan Carlos Yu <juancarlos.yu@yoyo-holdings.com>
2018-07-11 07:51:18 +0800

    Initial Commit

---
+++ Utilities/Utilities.h
@@ -0,0 +1,19 @@
+//
+//  Utilities.h
+//  Utilities
+//
+//  Created by Juan Carlos Yu on 11/07/2018.
+//  Copyright © 2018 Ximity Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Utilities.
+FOUNDATION_EXPORT double UtilitiesVersionNumber;
+
+//! Project version string for Utilities.
+FOUNDATION_EXPORT const unsigned char UtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utilities/PublicHeader.h>
+
+
